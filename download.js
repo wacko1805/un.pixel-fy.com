8344609821b2c38783ed63b1407d7ca60513ad3c
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2020-11-21 16:50:14 +1100

    Update download.js

--- download.js
+++ download.js
@@ -1,8 +1,8 @@
 const map = {
-    Base: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.5-no-MicroG/FOSSAPPSV1.5-No-MicroG.zip'
- ,MicroG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.5/FOSSAPPSV1.5.zip'
- ,Full: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.2-BETA-NO_MICROG/FOSSAPPSBETA-V0.2-BETA-No-MicroG.zip'
- ,FullMG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.2-BETA/FOSSAPPSBETA-V0.2-BETA.zip'
+    Base: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.6/FOSSAPPSV1.6-BASE.zip'
+ ,MicroG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.6-MicroG/FOSSAPPSV1.6-MicroG.zip'
+ ,Full: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.3-BETA/FOSSAPPSBETA-V0.3-BETA-Base.zip'
+ ,FullMG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.3-BETA-MicroG/FOSSAPPSBETA-V0.3-BETA-MicroG.zip'
 }
 document.getElementById('submit').addEventListener('click', () =>
   location.href = map[document.getElementById('select').value])
