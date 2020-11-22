9c8e0f64940cd16c5cfd2afa9071a7e57880cbe8
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2020-11-23 09:12:07 +1100

    Update download.js

--- download.js
+++ download.js
@@ -1,6 +1,6 @@
 const map = {
     Base: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.7/FOSSAPPSV1.7-BASE.zip'
- ,MicroG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.7-MicroG/FOSSAPPSV1.7-MicroG.zip'
+ ,MicroG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V1.7/FOSSAPPSV1.7-MicroG.zip'
  ,Full: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.3-BETA/FOSSAPPSBETA-V0.3-BETA-Base.zip'
  ,FullMG: 'https://github.com/wacko1805/FOSSAPPS/releases/download/V0.3-BETA-MicroG/FOSSAPPSBETA-V0.3-BETA-MicroG.zip'
 }
